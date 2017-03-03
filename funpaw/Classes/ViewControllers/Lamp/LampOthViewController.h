34c2755660f134a73615d81a15011fc555f3f731
yuleicoco <1040341999@qq.com>
2017-03-03 10:48:32 +0800

    no message

---
+++ funpaw/Classes/ViewControllers/Lamp/LampOthViewController.h
@@ -0,0 +1,13 @@
+//
+//  LampOthViewController.h
+//  funpaw
+//
+//  Created by yulei on 17/3/3.
+//  Copyright © 2017年 yulei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LampOthViewController : ViewController
+
+@end
---
+++ funpaw/Classes/ViewControllers/Lamp/LampOthViewController.m
@@ -0,0 +1,28 @@
+//
+//  LampOthViewController.m
+//  funpaw
+//
+//  Created by yulei on 17/3/3.
+//  Copyright © 2017年 yulei. All rights reserved.
+//
+
+#import "LampOthViewController.h"
+
+@interface LampOthViewController ()
+
+@end
+
+@implementation LampOthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
